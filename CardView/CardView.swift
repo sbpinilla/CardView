07bb92af90917022cac216d3e3f85a4bf683d04d
sbpinilla <sbpinilla@hotmail.com>
2018-01-10 21:14:30 -0500

    Commit inicial.

---
+++ CardView/CardView.swift
@@ -0,0 +1,31 @@
+//
+//  CardView.swift
+//  CardView
+//
+//  Created by Sergio on 10/01/18.
+//  Copyright © 2018 sergio. All rights reserved.
+//
+
+
+import UIKit
+
+class CardView: UIView {
+    
+    @IBInspectable var cornerRadius: CGFloat = 2
+    
+    @IBInspectable var shadowOffsetWidth: Int = 0
+    @IBInspectable var shadowOffsetHeight: Int = 3
+    @IBInspectable var shadowColor: UIColor? = UIColor.black
+    @IBInspectable var shadowOpacity: Float = 0.5
+    
+    override func layoutSubviews() {
+        layer.cornerRadius = cornerRadius
+        let shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius)
+        
+        layer.masksToBounds = false
+        layer.shadowColor = shadowColor?.cgColor
+        layer.shadowOffset = CGSize(width: shadowOffsetWidth, height: shadowOffsetHeight);
+        layer.shadowOpacity = shadowOpacity
+        layer.shadowPath = shadowPath.cgPath
+    }
+}
